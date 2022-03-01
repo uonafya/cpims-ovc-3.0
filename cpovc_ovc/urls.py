b1052e35f45799cd9c7f1ee2624849bda49ffa08
Nyiel <nyiel37@gmail.com>
2022-03-01 21:52:29 +0300

    Write Test case for urls, change sytax of both python and django from 2.7 and 1.8 to 3.10 to 4.0 respectivily, correct some HTML sytax error from the templates

--- cpovc_ovc/urls.py
+++ cpovc_ovc/urls.py
@@ -1,17 +1,18 @@
 """OVC care section urls."""
-from django.conf.urls import patterns, url
+from django.urls import path
 
 # This should contain urls related to registry ONLY
-urlpatterns = patterns(
+urlpatterns = [
     'cpovc_ovc.views',
-    url(r'^$', 'ovc_home', name='ovc_home'),
-    url(r'^ovc/search/$', 'ovc_search', name='ovc_search'),
-    url(r'^ovc/new/(?P<id>\d+)/$',
+    path(r'^$', 'ovc_home', name='ovc_home'),
+    path(r'^ovc/search/$', 'ovc_search', name='ovc_search'),
+    path(r'^ovc/new/(?P<id>\d+)/$',
         'ovc_register', name='ovc_register'),
-    url(r'^ovc/edit/(?P<id>\d+)/$',
+    path(r'^ovc/edit/(?P<id>\d+)/$',
         'ovc_edit', name='ovc_edit'),
-    url(r'^ovc/view/(?P<id>\d+)/$',
+    path(r'^ovc/view/(?P<id>\d+)/$',
         'ovc_view', name='ovc_view'),
-    url(r'^ovc/manage/$', 'ovc_manage', name='ovc_manage'),
-    url(r'^hh/view/(?P<hhid>[0-9A-Za-z_\-]+)/$',
-        'hh_manage', name='hh_manage'),)
+    path(r'^ovc/manage/$', 'ovc_manage', name='ovc_manage'),
+    path(r'^hh/view/(?P<hhid>[0-9A-Za-z_\-]+)/$',
+        'hh_manage', name='hh_manage'),
+    ]
