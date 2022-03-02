f3785ba8da1a5f2d82c4c254a6bd440a64bd9d33
Nyiel <nyiel37@gmail.com>
2022-03-02 14:34:02 +0300

    added test cases for views

--- cpovc_ovc/urls.py
+++ cpovc_ovc/urls.py
@@ -1,18 +1,16 @@
 """OVC care section urls."""
-from django.urls import path
+from django.urls import  path
 
 # This should contain urls related to registry ONLY
+from cpovc_ovc.views import ovc_register, ovc_edit, ovc_view
+
 urlpatterns = [
     'cpovc_ovc.views',
-    path(r'^$', 'ovc_home', name='ovc_home'),
-    path(r'^ovc/search/$', 'ovc_search', name='ovc_search'),
-    path(r'^ovc/new/(?P<id>\d+)/$',
-        'ovc_register', name='ovc_register'),
-    path(r'^ovc/edit/(?P<id>\d+)/$',
-        'ovc_edit', name='ovc_edit'),
-    path(r'^ovc/view/(?P<id>\d+)/$',
-        'ovc_view', name='ovc_view'),
-    path(r'^ovc/manage/$', 'ovc_manage', name='ovc_manage'),
-    path(r'^hh/view/(?P<hhid>[0-9A-Za-z_\-]+)/$',
-        'hh_manage', name='hh_manage'),
+    path('$', 'ovc_home', name='ovc_home'),
+    path('ovc/search/$', 'ovc_search', name='ovc_search'),
+    path('ovc/new/<int:id>/', ovc_register, name='ovc_register'),
+    path('ovc/edit/<int:id>/', ovc_edit, name='ovc_edit'),
+    path('ovc/view/<int:id>/', ovc_view, name='ovc_view'),
+    path('ovc/manage/$', 'ovc_manage', name='ovc_manage'),
+    path('hh/view/<hhid>[0-9A-Za-z_\-]+)/$', 'hh_manage', name='hh_manage'),
     ]
