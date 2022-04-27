78de743ea2540e6aa98737520f442d56921cdc8d
Cynthia Chebet <chebetcynthia8@gmail.com>
2022-04-27 17:52:57 +0300

    pre-post evaluation form

--- cpovc_forms/urls.py
+++ cpovc_forms/urls.py
@@ -174,4 +174,7 @@ urlpatterns = [
     # Dreams SerivceUptake Form
     re_path('dreamsform/new/(?P<id>\d+)/', views.new_dreamsform, name='new_dreamsform'),
 
+    # FMP Post-evaluation form
+    path('new_fmppostevaluation/new/<int:id>/', views.new_fmppostevaluation, name='new_fmppostevaluation'),
+
 ]
