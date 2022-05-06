ccf464eed75b2e48693eedd87c48693a16bfb8f1
abdimoha-dev <abdymohammed@gmail.com>
2022-05-06 19:27:30 +0300

    alter records

--- cpovc_forms/urls.py
+++ cpovc_forms/urls.py
@@ -177,9 +177,9 @@ urlpatterns = [
     path('preventiveattendanceregister/new/<int:id>/', views.preventive_attendance_register, name='preventiveattendanceregister'),
     path('preventiveattendanceregister/save/', views.save_preventive_register, name='save_preventive_register'),
     path('preventiveattendanceregister/manage/', views.manage_preventive_register, name='manage_preventive_register'),
-#     re_path('preventiveattendanceregister/delete_preventive_event_entry/<uuid:btn_event_pk>/<str:btn_event_type>/',views.delete_preventive_event_entry, name='delete_preventive_event_entry'),
     re_path('preventiveattendanceregister/delete/(?P<id>\d+)/(?P<btn_event_type>\w+)/(?P<btn_event_pk>.+)/', views.delete_preventive_event_entry,
             name='delete_preventive_event_entry'),
-
+    re_path('preventiveattendanceregister/edit/(?P<id>\d+)/(?P<btn_event_type>\w+)/(?P<btn_event_pk>.+)/', views.edit_preventive_event_entry,
+            name='edit_preventive_event_entry'),
 
 ]
