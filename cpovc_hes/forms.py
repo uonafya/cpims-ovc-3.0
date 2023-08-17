1ebb50e5df1cfad9dbc8243a31f92a80f98eae2d
Atieno-Ouma <atienooumavee@gmail.com>
2023-08-17 09:35:09 +0300

    fx:hes module cleanup

--- cpovc_hes/forms.py
+++ cpovc_hes/forms.py
@@ -27,11 +27,12 @@ YESNO_CHOICES = get_list('yesno_id')
 class HesForm(forms.Form):
     employment_status = forms.ChoiceField(label="Employment Status",
                                           choices=EMPLOYMENT_STATUS,
-                                          required=False,
+                                          required=True,
                                           widget=forms.Select(
                                               attrs={
                                                   "placeholder": ("Employment status"),
                                                   "class": "form-control",
+                                                  "data_parsley_required": "true",
 
                                               }
                                           ))
@@ -47,10 +48,16 @@ class HesForm(forms.Form):
                                            ))
 
     have_health_scheme = forms.ChoiceField(label='Have health scheme?',
+                                           required=True,
                                            choices=YESNO_CHOICES,
-                                           widget=forms.RadioSelect)
+                                           widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
 
     health_scheme = forms.ChoiceField(label="Health Scheme",
+                                      required=False,
                                       choices=HEALTH_SCHEME,
                                       widget=forms.Select(
                                           attrs={
@@ -60,12 +67,23 @@ class HesForm(forms.Form):
                                           }
                                       ))
     kitchen_garden = forms.ChoiceField(label="Kitchen Garden",
+                                       required=True,
                                        choices=YESNO_CHOICES,
-                                       widget=forms.RadioSelect)
+                                       widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     social_safety_nets = forms.ChoiceField(label="Social Safety Nets",
+                                           required=True,
                                            choices=YESNO_CHOICES,
-                                           widget=forms.RadioSelect)
+                                           widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     social_safety_nets_type = forms.ChoiceField(label="Social Safety Nets",
+                                                required=False,
                                                 choices=SAFETY_NETS,
                                                 widget=forms.Select(
                                                     attrs={
@@ -76,26 +94,33 @@ class HesForm(forms.Form):
                                                 ))
 
     linkage_to_vsls = forms.ChoiceField(label="Linkage to VSLS",
+                                        required=True,
                                         choices=YESNO_CHOICES,
-                                        widget=forms.RadioSelect)
+                                        widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     vsla_name = forms.CharField(label="VSLA",
+                                required=False,
                                 widget=forms.TextInput(
                                     attrs={'placeholder': ('VSLA'),
                                            'class': 'form-control',
                                            }))
 
     date_linkage = forms.DateField(label='Date of Linkage to VSLA',
+                                   required=False,
                                    widget=forms.TextInput(
                                        attrs={
                                            "placeholder": ("Date Start Up Received"),
                                            "class": "form-control",
                                            "id": "admission_date",
-                                           "data_parsley_required": "true",
                                            "data_parsley_group": "group0",
                                        }
                                    )
                                    )
     monthly_saving = forms.DecimalField(label='Monthly Saving',
+                                        required=False,
                                         widget=forms.TextInput(
                                             attrs={'placeholder': ('Amount(Ksh)'),
                                                    'class': 'form-control',
@@ -103,6 +128,7 @@ class HesForm(forms.Form):
 
                                                    }))
     average_cumulative_saving = forms.DecimalField(label='Average Cumulative Saving',
+                                                   required=False,
                                                    widget=forms.TextInput(
                                                        attrs={'placeholder': ('Amount(Ksh)'),
                                                               'class': 'form-control',
@@ -110,26 +136,33 @@ class HesForm(forms.Form):
 
                                                               }))
     loan_taken = forms.ChoiceField(label='Loan Taken',
+                                   required=True,
                                    choices=YESNO_CHOICES,
-                                   widget=forms.RadioSelect)
+                                   widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     loan_taken_amount = forms.DecimalField(label='Loan Taken Amount',
+                                           required=False,
                                            widget=forms.TextInput(
                                                attrs={'placeholder': ('Amount(Ksh)'),
                                                       'class': 'form-control',
 
                                                       }))
     date_loan_taken = forms.DateField(label='Date Loan Taken',
+                                      required=False,
                                       widget=forms.TextInput(
                                           attrs={
                                               "placeholder": ("Date Start Up Received"),
                                               "class": "form-control",
                                               "id": "admission_date",
-                                              "data_parsley_required": "true",
                                               "data_parsley_group": "group0",
                                           }
                                       )
                                       )
     loan_utilization = forms.ChoiceField(label='Loan Utilization',
+                                         required=False,
                                          choices=LOAN_UTILIZATION,
                                          widget=forms.Select(
                                              attrs={'class': 'form-control',
@@ -137,34 +170,49 @@ class HesForm(forms.Form):
 
                                                     }))
     startup = forms.ChoiceField(label='Start Up',
+                                required=True,
                                 choices=YESNO_CHOICES,
-                                widget=forms.RadioSelect)
+                                widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
 
     type_of_startup = forms.ChoiceField(label='Type of Startup',
+                                        required=False,
                                         choices=SECTOR,
                                         widget=forms.Select(
                                             attrs={'class': 'form-control',
 
                                                    }))
     date_startup_received = forms.DateField(label='Date Startup Received',
+                                            required=False,
                                             widget=forms.TextInput(
                                                 attrs={
                                                     "placeholder": ("Date Start Up Received"),
                                                     "class": "form-control",
                                                     "id": "admission_date",
-                                                    "data_parsley_required": "true",
                                                     "data_parsley_group": "group0",
                                                 }
                                             )
                                             )
     emergency_cash_transfer = forms.ChoiceField(label='Emergency Cash Transfer (ECT)',
+                                                required=True,
                                                 choices=YESNO_CHOICES,
-                                                widget=forms.RadioSelect)
+                                                widget=forms.RadioSelect
+                                                    (
+                                                    attrs={
+                                                        "data_parsley_required": "true",
+                                                    }
+                                                )
+                                                )
     amount_received_ect = forms.DecimalField(label='Amount received ECT',
+                                             required=False,
                                              widget=forms.TextInput(
                                                  attrs={'placeholder': ('Amount(Ksh)'),
                                                         'class': 'form-control'}))
     use_of_ect = forms.ChoiceField(label='Use of ECT',
+                                   required=False,
                                    choices=USE_OF_ECT,
                                    widget=forms.Select(
                                        attrs={'class': 'form-control',
@@ -172,69 +220,106 @@ class HesForm(forms.Form):
                                               }))
 
     received_startup_kit = forms.ChoiceField(label='Received Start-up Kit',
+                                             required=True,
                                              choices=YESNO_CHOICES,
-                                             widget=forms.RadioSelect)
+                                             widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     type_of_asset = forms.ChoiceField(label='Type of Asset',
+                                      required=False,
                                       choices=TYPE_OF_ASSETS,
                                       widget=forms.Select(
                                           attrs={'class': 'form-control',
 
                                                  }))
     average_monthly_income_generated = forms.DecimalField(label='Average Monthly Income Generated',
+                                                          required=False,
                                                           widget=forms.TextInput(
                                                               attrs={'class': 'form-control',
 
                                                                      }))
     received_business_grant = forms.ChoiceField(label='Received Business Grant',
+                                                required=True,
                                                 choices=YESNO_CHOICES,
-                                                widget=forms.RadioSelect)
+                                                widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     amount_of_money_received = forms.DecimalField(label='Amount of Money Received',
+                                                  required=False,
                                                   widget=forms.TextInput(
                                                       attrs={'class': 'form-control',
 
                                                              }))
     business_type_started = forms.CharField(label='Business Type Started',
+                                            required=False,
                                             widget=forms.TextInput(
                                                 attrs={'placeholder': ('business type'),
                                                        'class': 'form-control',
                                                        }))
     linked_to_value_chain_activities_asset_growth = forms.ChoiceField(label='Linked to',
+                                                                      required=True,
                                                                       choices=YESNO_CHOICES,
-                                                                      widget=forms.RadioSelect)
+                                                                      widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     sector_of_asset_growth = forms.ChoiceField(label='Sector',
+                                               required=False,
                                                choices=SECTOR,
                                                widget=forms.Select(
                                                    attrs={'class': 'form-control',
 
                                                           }))
     linked_to_source_finance = forms.ChoiceField(label='Linked to Source of Finance',
+                                                 required=True,
                                                  choices=YESNO_CHOICES,
-                                                 widget=forms.RadioSelect)
+                                                 widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
 
     type_of_financial_institution = forms.ChoiceField(label='Type of Financial Institution',
+                                                      required=False,
                                                       choices=FINANCIAL_INSTITUTION_TYPE,
                                                       widget=forms.Select(
                                                           attrs={'class': 'form-control',
 
                                                                  }))
     loan_taken_income_growth = forms.ChoiceField(label='Loan Taken',
+                                                 required=True,
                                                  choices=YESNO_CHOICES,
-                                                 widget=forms.RadioSelect)
+                                                 widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     date_loan_taken_income_growth = forms.DateField(label='Date of Loan Taken',
+                                                    required=False,
                                                     widget=forms.TextInput(
                                                         attrs={
                                                             "placeholder": ("Date Start Up Received"),
                                                             "class": "form-control",
                                                             "id": "admission_date",
-                                                            "data_parsley_required": "true",
                                                             "data_parsley_group": "group0",
                                                         }
                                                     )
                                                     )
     linked_to_value_chain_activities_income_growth = forms.ChoiceField(label='Linked to',
+                                                                       required=True,
                                                                        choices=YESNO_CHOICES,
-                                                                       widget=forms.RadioSelect)
+                                                                       widget=forms.RadioSelect(
+                                               attrs={
+                                                   "data_parsley_required": "true",
+                                               }
+                                           ))
     sector_of_income_growth = forms.ChoiceField(label='Sector',
+                                                required=False,
                                                 choices=SECTOR,
                                                 widget=forms.Select(
                                                     attrs={'class': 'form-control',
