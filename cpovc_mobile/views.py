bd8d0d224fdda5af8dbba94c35652bb07cf15bb5
Mugwe B <102720427+bmugwe@users.noreply.github.com>
2023-11-01 14:10:36 +0300

    approval messages to UI

--- cpovc_mobile/views.py
+++ cpovc_mobile/views.py
@@ -965,9 +965,12 @@ def mobile_home(request):
 
         chvss = OVCRegistration.objects.filter(is_void=False, child_cbo_id=lip_id).distinct('child_chv_id')
         care_quiz = OVCCareQuestions.objects.filter(is_void=False, code__startswith="CP")
-        cpt_fields = ['case_plan_services_school', 'case_plan_services_safe', 'case_plan_services_stable', 'case_plan_services_health', 'case_plan_goals_school', 'case_plan_goals_safe', 'case_plan_goals_stable', 'case_plan_goals_health', 'case_plan_gaps_school', 'case_plan_gaps_safe', 'case_plan_gaps_stable', 'case_plan_gaps_health', 'case_plan_priorities_school', 'case_plan_priorities_safe', 'case_plan_priorities_stable', 'case_plan_priorities_health', 'ovc_domain_id']
+        cpt_fields = ['case_plan_services_school', 'case_plan_services_safe', 'case_plan_services_stable', 'case_plan_services_health', 
+                      'case_plan_goals_school', 'case_plan_goals_safe', 'case_plan_goals_stable', 'case_plan_goals_health', 'case_plan_gaps_school', 'case_plan_gaps_safe', 
+                      'case_plan_gaps_stable', 'case_plan_gaps_health', 'case_plan_priorities_school', 'case_plan_priorities_safe', 'case_plan_priorities_stable', 'case_plan_priorities_health', 'ovc_domain_id']
         cpt_list = get_dict(field_name=cpt_fields)
-        f1b_fields = ['form1b_items', 'olmis_domain_id', 'olmis_protection_service_id', 'olmis_hes_service_id', 'olmis_health_service_id', 'olmis_shelter_service_id', 'olmis_pss_service_id', 'olmis_education_service_id', 'olmis_critical_event_id', 'caregiver_critical_event_id']
+        f1b_fields = ['form1b_items', 'olmis_domain_id', 'olmis_protection_service_id', 'olmis_hes_service_id', 'olmis_health_service_id', 'olmis_shelter_service_id',
+                       'olmis_pss_service_id', 'olmis_education_service_id', 'olmis_critical_event_id', 'caregiver_critical_event_id']
         f1b_list = get_dict(field_name=f1b_fields)
         # f1a_list = get_dict([''])
 
